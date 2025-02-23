d2be11e01902db352102f0b5168478db71f7f691
Dhrubo <aaa.dhrubo@gmail.com>
2025-02-23 17:15:48 -0500

    add card UI component

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,11 +1,12 @@
 import { Component, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Task } from './../task/task.model';
+import { CardComponent } from "../../shared/card/card.component";
 import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-new-task',
-  imports: [FormsModule],
+  imports: [FormsModule, CardComponent],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
